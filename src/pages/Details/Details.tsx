d14fa3dff9247ef50113d299f0f2ec081d0f89f5
fernando predes <fernando_predes@hotmail.com>
2023-04-04 15:23:04 -0300

    função de deletar ROB funcionando

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import api from '../../api';
-import { DetailsPage, Filled } from './DetailsStyle';
+import { BtnDelete, DetailsPage, Filled, GreenScreen } from './DetailsStyle';
+import { BtnGoBack } from '../../components/RegisterForm/RegisterFormStyle';
 
 
 type DailyRecord = {
@@ -52,6 +53,26 @@ const Details = () => {
     }
   }, [])
 
+  /* Função de deletar ROB */
+  const navigate = useNavigate();
+  const deleteDailyRecord = async (id:any) => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await api.delete(`/daily_record/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+      alert('ROB deletado sucesso.')
+        navigate('/')
+        window.location.reload()
+    } catch (error) {
+      console.error(error);
+
+    }
+  };
+
   /* Valores máximos de cada tanque */
   const maxDiesel = 2000
   const maxFreshWater = 1000
@@ -81,56 +102,64 @@ const Details = () => {
 
   return (
     <DetailsPage>
-      <ul>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='diesel'>{maxDiesel} (m³)</span>
-          <div className="square">
-            <Filled filledHeight={dieselFilledHeight} productType={'#230808'}/>
-          </div>
-          <span>Diesel {record.diesel} (m³)</span>
-        </li>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='fw'>{maxFreshWater} (m³)</span>
-          <div className="square">
-            <Filled filledHeight={freshWaterFilledHeight} productType={'#0085FF'}/>
-          </div>
-          <span>Fresh Water {record.fresh_water} (m³)</span>
-        </li>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='fw'>{maxDrillWater} (m³)</span>
-          <div className="square">
-            <Filled filledHeight={drillWaterFilledHeight} productType={'#0085FF'}/>
-          </div>
-          <span>Drill Water {record.drill_water} (m³)</span>
-        </li>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='ben'>{maxBarite} (ft³)</span>
-          <div className="square">
-            <Filled filledHeight={bariteFilledHeight} productType={'#6E727D'}/>
-          </div>
-          <span>Baritina {record.barite} (ft³)</span>
-        </li>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='ben'>{maxBentonite} (ft³)</span>
-          <div className="square">
-            <Filled filledHeight={bentoniteFilledHeight} productType={'#6E727D'} />
-          </div>
-          <span>Bentonita {record.bentonite} (ft³)</span>
-        </li>
-        <li>
-          <span>Capacidade Máx.</span>
-          <span id='lime'>{maxLimestone} (ft³)</span>
-          <div className="square">
-            <Filled filledHeight={limestoneFilledHeight} productType={'#6E727D'}/>
-          </div>
-          <span>Calcário: {record.limestone} (ft³)</span>
-        </li>
-      </ul>
+      <GreenScreen>
+        <h1>ROB - {record.date}</h1>
+        <ul className='tanks'>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='diesel'>{maxDiesel} (m³)</span>
+            <div className="square">
+              <Filled filledHeight={dieselFilledHeight} productType={'#230808'}/>
+            </div>
+            <span>Diesel {record.diesel} (m³)</span>
+          </li>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='fw'>{maxFreshWater} (m³)</span>
+            <div className="square">
+              <Filled filledHeight={freshWaterFilledHeight} productType={'#0085FF'}/>
+            </div>
+            <span>Fresh Water {record.fresh_water} (m³)</span>
+          </li>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='fw'>{maxDrillWater} (m³)</span>
+            <div className="square">
+              <Filled filledHeight={drillWaterFilledHeight} productType={'#0085FF'}/>
+            </div>
+            <span>Drill Water {record.drill_water} (m³)</span>
+          </li>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='ben'>{maxBarite} (ft³)</span>
+            <div className="square">
+              <Filled filledHeight={bariteFilledHeight} productType={'#6E727D'}/>
+            </div>
+            <span>Baritina {record.barite} (ft³)</span>
+          </li>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='ben'>{maxBentonite} (ft³)</span>
+            <div className="square">
+              <Filled filledHeight={bentoniteFilledHeight} productType={'#6E727D'} />
+            </div>
+            <span>Bentonita {record.bentonite} (ft³)</span>
+          </li>
+          <li>
+            <span>Capacidade Máx.</span>
+            <span id='lime'>{maxLimestone} (ft³)</span>
+            <div className="square">
+              <Filled filledHeight={limestoneFilledHeight} productType={'#6E727D'}/>
+            </div>
+            <span>Calcário: {record.limestone} (ft³)</span>
+          </li>
+        </ul>
+        <ul className='buttons'>
+          <BtnGoBack to={'/'}>voltar</BtnGoBack>
+          <BtnGoBack to={'/'}>editar</BtnGoBack>
+          <BtnDelete onClick={() => {deleteDailyRecord(id)}}>deletar</BtnDelete>
+        </ul>
+      </GreenScreen>
     </DetailsPage>
   )
 }
--- src/pages/Details/DetailsStyle.ts
+++ src/pages/Details/DetailsStyle.ts
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components"
 
 interface FilledProps {
@@ -15,13 +16,35 @@ export const Filled = styled.div<FilledProps>`
   background-color: ${(props) => props.productType};
 `;
 
+export const GreenScreen = styled.div `
+  background: #9EC496;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 12px;
+  max-width:1170px;
+  position: relative;
+  padding: 40px 0;
+  margin: 10px auto 0 auto;
+  h1{
+    font-size:40px;
+    text-align:center;
+    font-family: "Century Gothic Bold";
+  }
+  .buttons{
+    display: flex;
+    justify-content:space-around;
+  }
+`
+
 export const DetailsPage = styled.div `
-ul{
+.tanks{
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 20px;
+  gap: 45px;
   margin-top: 20px;
   margin-bottom: 50px;
+  span{
+    margin-bottom: 1rem;
+  }
   li{
     display: flex;
     flex-direction:column;
@@ -50,7 +73,7 @@ ul{
       width: 110px;
       height: 147px;
       border: 2px solid #000000;
-
+      margin-bottom:1.2rem ;
       transform: scaleY(-1);
       .filled{
         position: absolute;
@@ -60,4 +83,20 @@ ul{
     }
   }
 }
+`
+
+export const BtnDelete = styled.button`
+  background: #E01B1B;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
+  font-family: 'Century Gothic';
+  font-weight: 700;
+  font-size: 12.8px;
+  line-height: 16px;
+  letter-spacing: 0.4em;
+  text-transform: uppercase;
+  color: #FFFFFF;
+  padding: 15px 45px;
+  border: none;
+  cursor: pointer;
 `
